dde5088c1e50976a84eb20f7a6b14119bef2dc17
Rebecca Turner <637275@gmail.com>
2017-08-07 08:30:34 -0400

    trashed eskymap, fixed list_chars?

--- uni2esky/list_chars.py
+++ uni2esky/list_chars.py
@@ -1,5 +1,26 @@
 #! /usr/local/bin/python3
 from uni2esky import eskymap
 
-for c in sorted(list(eskymap.chars)):
-    print(hex(c) + ' = ' + chr(c))
+def char_list(style='short'):
+    ret = []
+    if style == 'short':
+        ret.extend(hex(c), ' = ', chr(c))
+    else:
+        ret.extend(chr(c))
+    return ''.join(ret)
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Lists codepoints in uni2esky.eskymap'
+    )
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+        help='Shows codepoints as well as characters')
+
+    args = parser.parse_args()
+
+    char_list(style='long' if args.verbose else 'short')
+
+if __name__ == '__main__':
+    main()
